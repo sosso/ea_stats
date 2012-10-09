5e311d90eb86e8966f1acf76baaf2cdcf8f08717
Anthony Sosso <sosso@vt.edu>
2012-10-09 16:54:33 -0400

    back to urllib2

--- download_ea_page.py
+++ download_ea_page.py
@@ -1,17 +1,16 @@
-from google.appengine.api import urlfetch
 import datetime
+import sys
 import time
+import urllib2
 
 def download_ea_page():
-    stats_page = urlfetch.fetch(url="http://www.easportsworld.com/en_US/clubs/partial/401A0001/273/members-list",
-                                method=urlfetch.GET)
-    if stats_page.status_code == 200:
-        output = open('stats_current.html', 'wb')
-        output.write(stats_page.content)
-        output.close()
-        output2 = open('%s.html' % time.mktime(datetime.datetime.now().timetuple(), 'wb'))
-        output2.write(stats_page)
-        output2.close()
-        return 'Downloaded stats'
-    else:
-        return 'Failed'
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    req = urllib2.Request('http://www.easportsworld.com/en_US/clubs/partial/401A0001/273/members-list', None, headers)
+    stats_page = urllib2.urlopen(req, None, 30).read()
+    output = open('stats_current.html', 'wb')
+    output.write(stats_page.content)
+    output.close()
+    output2 = open('%s.html' % time.mktime(datetime.datetime.now().timetuple(), 'wb'))
+    output2.write(stats_page)
+    output2.close()
+    return 'Downloaded stats'
