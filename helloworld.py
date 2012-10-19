d9819a70a79954a7f8b4e1ac9caaea13afeb35df
Anthony Sosso <asosso@friendeo.com>
2012-10-19 15:41:44 -0400

    directory listing

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import download_ea_page
 import os
 
@@ -16,6 +16,20 @@ def fetch_stats():
 #    self.response.headers['Content-Type'] = 'text/plain'
 #    self.response.out.write(download_ea_page.download_ea_page())
     return download_ea_page.download_ea_page()
+
+@app.route('/oldstats')
+def serve_old_stats():
+    oldpages = os.listdir('static')
+    oldpages.sort()
+    if request.args['filename'] in oldpages:
+        return open('static/%s' % request.args['filename']).read()
+    else:
+        links_html = ''
+        for filename in oldpages:
+            links_html += '<a href=http://ea-stats.herokuapp.com/oldstats?filename="%s">%s</a><br>' % (filename, filename)
+
+        return_html = '<html><body>' + links_html + '<body></html>'
+        return return_html
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
